93103fad22027c04f40dd8c11bd6d4d6b73508fd
Elvis Gonzalez <ElvisJGonzalez@gmail.com>
2019-08-31 18:44:18 -0400

    progress, will pick this up tomorrow

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,6 @@
 import firebase from 'firebase/app';
 import 'firebase/firestore';
 
-const firebaseConfig = firebase.initializeApp({
-  apiKey: 'AIzaSyCNIVDUKKHijGe3hbe2nynGRsJ4jXFR8Nk',
-  authDomain: 'todoist-clone-2760f.firebaseapp.com',
-  databaseURL: 'https://todoist-clone-2760f.firebaseio.com',
-  projectId: 'todoist-clone-2760f',
-  storageBucket: 'todoist-clone-2760f.appspot.com',
-  messagingSenderId: '126404894789',
-  appId: '1:126404894789:web:1cc6087e83026ed5'
-});
+const firebaseConfig = firebase.initializeApp({});
 
 export { firebaseConfig as firebase };
