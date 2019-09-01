04f7ed43d884d6ad4fd841e44eeb6f5d081639c9
Elvis Gonzalez <ElvisJGonzalez@gmail.com>
2019-09-01 15:14:57 -0400

    comments and further optimization

--- src/context/darkmode-context.js
+++ src/context/darkmode-context.js
@@ -2,29 +2,31 @@ import React, { createContext, useContext, useState } from 'react';
 
 export const DarkModeContext = createContext();
 export const DarkModeProvider = ({ children }) => {
+  // This function returns a boolean value after evaluation
   function getTheme() {
     const returning = 'WeDark?' in localStorage;
     const theme = JSON.parse(localStorage.getItem('WeDark?'));
     const userPrefersDark = matchMedia();
-    // if mode was saved, return dark or light
+    // If a returning user has saved a prefered mode -> return the saved theme
     if (returning) {
       return theme;
-      // if preferred scheme is dark, return dark
+      // If there is no token: check if preferred scheme is dark if so -> return dark
     } else if (userPrefersDark) {
       return true;
-      // otherwise, return light
+      // otherwise -> return light (Default Theme)
     } else {
       return false;
     }
   }
 
+  // This function checks the Window interface's MatchMedia() method to see if
+  // a user prefers dark themes
   function matchMedia() {
     if (!window.matchMedia) return;
     return window.matchMedia('(prefers-color-scheme: dark)').matches;
   }
 
   const [darkMode, setDarkMode] = useState(getTheme());
-
   return (
     <DarkModeContext.Provider value={{ darkMode, setDarkMode }}>
       {children}
